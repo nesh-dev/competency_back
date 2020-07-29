281397f2774fec751815a174f84f12b4b277dfcf
paulmburu08 <paulmburu08@gmail.com>
2020-07-29 11:44:41 +0300

    feature(assessment):Assessment for Reportee and Manager
    -Competency model for various fields of assessment created
    -Strands model for the subsets of fields of assessment created
    -Assessment model for interger values when assessed created
    -End point for get,post,put and delete competencies done
    -End point for get,post,put and delete strands of competencies done
    -End point for get,post,put of assessments done
    -End point for getting the average of different compitencies
    
    [Starts #173923361]

--- apps/assessment/urls.py
+++ apps/assessment/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     url(r'^assessments/list/$', views.AssessmentList.as_view()),
     url(r'^assessee/(\d+)/$', views.SelfAssessment.as_view()),
     url(r'^assesser/(\d+)/$', views.AssesserAssessment.as_view()),
+    path('assesser/average/<int:pk>/<int:id1>/<int:id>/', views.GetAssesserAverage.as_view()),
+    path('assessee/average/<int:pk>/<int:id>/', views.GetAssesseeAverage.as_view()),
+    path('assessee/average/list/<int:id>/', views.SelfAssessmentAverageList.as_view()),
+    path('assesser/average/list/<int:id>/', views.AssessmentDoneAverageList.as_view()),
 ]
